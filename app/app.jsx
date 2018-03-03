48b9ca3348d34a0fd3c1e153fee52528326adebf
RAKESH <rakeshgn97@gmail.com>
2018-03-03 16:01:50 +0530

    Init commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+// App css
+require('style!css!applicationStyles')
+
+ReactDOM.render(
+  <p>Boiler Plate 3 Project</p>,
+  document.getElementById('app')
+);
